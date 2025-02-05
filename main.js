465465870930c9f1f3f04b0d594a1cd2543765d7
MD. ELIUS <mdelius.nfs@gmail.com>
2025-02-05 07:49:13 +0600

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('Hello World!');
+
+
+
+const menuIcon = document.getElementById("menu_icon");
+
+const menu = document.getElementById("menu");
+
+menuIcon?.addEventListener("click", ()=> {
+    if(menu?.className === "hidden"){
+    menu.classList.remove("hidden");
+    }else{ menu.classList.add("hidden");
+}
+  });
+
+
+
+  
+
